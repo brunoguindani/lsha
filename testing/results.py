fa193ef481156910d03c8551be70fad748a475cb
brunoguindani <bruno@guindani.org>
2025-04-18 14:26:54 +0200

    Refactoring of test results script

--- testing/results.py
+++ testing/results.py
@@ -25,40 +25,41 @@ def is_realistic(row: pd.Series) -> bool:
       return False
   return True
 
-def filter_and_count(df: pd.DataFrame, func: Callable | None,
-                     print_prefix: str = "") -> pd.DataFrame:
-  if func is not None:
-    df_filtered = df.loc[ df.apply(func, axis=1) ]
-  else:
-    df_filtered = df
+def filter(df: pd.DataFrame, func: Callable) -> pd.DataFrame:
+  return df.loc[ df.apply(func, axis=1) ]
 
-  sizes = df_filtered.groupby('seed').size()
+def count(df: pd.DataFrame, print_prefix: str = ""):
+  sizes = df.groupby('seed').size()
   mean = sizes.mean()
   std = sizes.std()
   print(print_prefix, mean, "±", round(std, 2))
-  return df_filtered
+  # print(list(sizes))
 
-def compare_given_threshold(df: pd.DataFrame, threshold_prob: float):
+def compare_given_threshold(input_df: pd.DataFrame, threshold_prob: float):
   prob_to_bool = lambda p : int(p >= threshold_prob)
+  df = input_df.copy()
   df[query_cols] = df[query_cols].map(prob_to_bool)
   df[all_transition_cols] = df[all_transition_cols].astype(int)
 
-  for tec, group in df.groupby('technique'):
-    print(tec)
-    filter_and_count(group, None, "Total size")
-    filter_and_count(group, is_defect, "Total defects")
-
-    for query in query_cols:
-      print(query)
-      group_defects = group.loc[ group[query] == 1 ]
-      filter_and_count(group_defects, None, "Defects")
-      filter_and_count(group_defects, is_realistic, "of which realistic")
+  # Loop over queries
+  for query in query_cols:
+    print("Requirement:", query)
+    # Loop over techniques
+    for tec, group in df.groupby('technique'):
+      # Count number of iterations
+      print("Technique", tec)
+      # count(group, " Total size")
+      # Filter on mutants that violate query
+      group_def = group.loc[ group[query] == 1 ]
+      count(group_def, " Defects")
+      group_def_real = filter(group_def, is_realistic)
+      count(group_def_real, " of which realistic")
     print()
 
 
 if __name__ == '__main__':
   df = pd.read_csv('testing_2seeds_test.csv')
   for p in [0.5, 0.75, 0.9]:
-    print("Threshold =", p)
+    print(20*"-", "\nThreshold =", p)
     compare_given_threshold(df, p)
-    print(10*"-")
+    print(20*"-")
